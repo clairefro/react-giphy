87958185094795464ce2fb6d6ba665b36cda7174
claire froelich <claire.froelich@gmail.com>
2019-12-29 13:22:52 -0500

    change props to state for changing components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,24 +6,34 @@ import GifList from './gif_list.jsx';
 import BGCred from './svq-bg-cred.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // define changing components
+    this.state = {
+      gifs: [],
+      selectedGifId: "l41lLVwbg5bfykLPa"
+    }
+  }
+
   render() {
     // define gif array
-    const gifs = [
-      { id: "l41lLVwbg5bfykLPa" },
-      { id: "YmDNxawTergvm"},
-      { id: "59QDqizvIzida"}
-    ]
+    // const gifs = [
+    //   { id: "l41lLVwbg5bfykLPa" },
+    //   { id: "YmDNxawTergvm"},
+    //   { id: "59QDqizvIzida"}
+    // ]
     return (
       <div>
         <div className="left-scene" >
           <SearchBar />
           <div className="selected-gif">
-            <Gif id="l41lLVwbg5bfykLPa"/>
+            <Gif id={this.state.selectedGifId}/>
             <BGCred />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs}/>
+          <GifList gifs={this.state.gifs}/>
         </div>
       </div>
     );
