7734b4e6845201ca1b39e8af89bede704e33aefa
claire froelich <claire.froelich@gmail.com>
2019-12-29 13:14:06 -0500

    refactor to single line implicit return

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -4,10 +4,7 @@ import Gif from './gif.jsx';
 
 class GifList extends Component {
   renderList = () => {
-    return this.props.gifs.map((gif) => {
-      return <Gif id={gif.id} key={gif.id} />
-    })
-
+    return this.props.gifs.map(gif =>  <Gif id={gif.id} key={gif.id} />);
   }
 
   render() {
