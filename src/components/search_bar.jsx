ea4b14469aec6faeb5bf80866b8215055a0629db
claire froelich <claire.froelich@gmail.com>
2019-12-30 13:47:53 -0500

    added controlled component functionality to search bar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ""
+    };
+  }
+
   handleUpdate = (e) => {
+    this.setState({
+      term: e.target.value
+    });
     this.props.search(e.target.value);
   }
 
   render() {
     return (
       <input
+        value={this.state.term}
         type="text"
         className="form-control form-search"
         placeholder="Search gifs..."
